e82149c4c2f0c11c59ebd1c924c565dedc4c1780
Jiaming Guo <j23guo@gmail.com>
2023-01-18 20:16:12 -0500

    Initial Commit

---
+++ NetworkDemo/NetworkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDemoApp.swift
+//  NetworkDemo
+//
+//  Created by Jiaming Guo on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
