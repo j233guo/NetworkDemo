43fccf07e32bb898e504b1359607d7b81b1c8015
Jiaming Guo <j23guo@gmail.com>
2023-01-22 17:51:23 -0500

    added alamo fire package

--- NetworkDemo/ContentView.swift
+++ NetworkDemo/ContentView.swift
@@ -5,15 +5,41 @@
 //  Created by Jiaming Guo on 2023-01-18.
 //
 
+import Alamofire
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+    
+    func load() {
+        let url = URL(string: "https://raw.githubusercontent.com/j233guo/PostWeibo/main/PostWeibo/Resources/PostListData_recommend_1.json")!
+        AF.request(url).responseData { response in
+            
+        }
+    }
+    
+    func updateText(_ text: String) {
+        self.text = text
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(text)
+                .font(.title)
+            
+            Button {
+                load()
+            } label: {
+                Text("Start")
+                    .font(.largeTitle)
+            }
+            
+            Button {
+                text = ""
+            } label: {
+                Text("Clear")
+                    .font(.largeTitle)
+            }
         }
         .padding()
     }
